13c198d0e7d14393d2cb0fda0ce1520fba713f31
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-08 16:07:45 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
-sudo tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
+tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
 export JMETER_HOME=/opt/apache-jmeter-5.5
 export JMETER_BIN=${JMETER_HOME}/bin
 export PATH=$PATH:$JMETER_BIN
