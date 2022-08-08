2cae68e1d29ffffdaa67c3d713d3e7d18d21ae75
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-08 14:45:43 +0400

    Create run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
+sudo tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
+export JMETER_HOME=/opt/apache-jmeter-5.5
+export JMETER_BIN=${JMETER_HOME}/bin
+export PATH=$PATH:$JMETER_BIN
+sudo amazon-linux-extras install java-openjdk11 -y
+export PATH=$PATH:$JMETER_BIN
+jmeter --version
