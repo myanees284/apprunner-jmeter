364d3ddc63a70fb8487037dd79be119afd20cf53
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-18 13:15:13 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+yum update -y
+yum install awscli -y
 curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
 yum -y install tar
 tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
@@ -6,8 +8,9 @@ export JMETER_HOME=/opt/apache-jmeter-5.5
 export JMETER_BIN=${JMETER_HOME}/bin
 export PATH=$PATH:$JMETER_BIN
 export PATH=$PATH:$JMETER_BIN
-echo jmeter -n -t $1.jmx -l $1.csv -e -o $1
-jmeter -n -t my_test.jmx -l $1.csv -e -o $1
-mv $1.csv $1/
-#yum -y install httpd
-#/usr/sbin/httpd
+echo jmeter -n -t $1.jmx -l $1_result.csv -e -o $1
+jmeter -n -t my_test.jmx -l $1_result.csv -e -o $1
+mv $1_result.csv $1/
+aws s3 cp $1/ s3://loveisair/$1-html-report --recursive
+yum -y install httpd #dummy step
+httpd #dummy step
