2270e457d79e3a399abb19645e78ee29e5796800
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-18 14:28:57 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
-echo jmeter -n -t $1.jmx -l $1_result.csv -e -o $1
+rName=$(date +%F)_$(date +%s)
+echo "*******************Downloading JMeter*******************"
 curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
 yum -y install tar
 tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
 export JMETER_HOME=/opt/apache-jmeter-5.5
 export JMETER_BIN=${JMETER_HOME}/bin
 export PATH=$PATH:$JMETER_BIN
-export PATH=$PATH:$JMETER_BIN
+echo "*******************Starting load test*******************"
 jmeter -n -t my_test.jmx -l $1_result.csv -e -o $1
+echo "*******************Test Complete*******************"
 mv $1_result.csv $1/
 yum install awscli -y
-aws s3 cp $1/ s3://loveisair/$1-html-report --recursive
-yum -y install httpd #dummy step
+echo "*******************Copying Results into S3 bucket*******************"
+aws s3 cp $1/ s3://loveisair/$1-$rName_html_report --recursive
+echo "*******************Installing apache*******************"
+yum -y install httpd
+echo "*******************Running Apache*******************"
 httpd #dummy step
