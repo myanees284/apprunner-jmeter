22f94bdc41fa5338500b62f73af20f11663e1e21
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-21 13:04:54 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -20,3 +20,4 @@ mv $1_result.csv $1/
 yum install awscli -y
 echo "*******************Copying Results into S3 bucket*******************"
 aws s3 cp $1/ s3://loveisair/$1-$rName-html-report --recursive
+exit
