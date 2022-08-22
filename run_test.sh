7b756f780ab9d398da174e2da9cdb04d25e3b88c
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-22 23:00:33 +0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,7 @@ yum -y install nc
 while true; do { \
   echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; \
   cat index.html; } | nc -l -p 8080 ; \ 
-done
+done &
 rName=$(date +%F)_$(date +%s)
 echo "*******************Downloading JMeter*******************"
 curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
