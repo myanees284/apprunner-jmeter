35f75fd2efbe2b5840c05c0863a313ecde2ac224
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2022-08-22 22:18:07 +0400

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 yum -y install nc
-while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo "Hello World"} | nc -l 8080; done
+while true; do { \
+  echo -e 'HTTP/1.1 200 OK\r\n'; \
+  echo "Hello World"} | nc -l 8080; \
+done
