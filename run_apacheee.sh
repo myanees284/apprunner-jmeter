d2d2499f512a2b5ba673a92370c77884b553606b
EC2 Default User <ec2-user@ip-13-0-0-193.us-east-2.compute.internal>
2022-09-02 08:10:27 +0000

    code refactored

--- run_apacheee.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-yum -y install httpd
-httpd -k start
-sleep 5
-echo "--------apache server started--------"
--- run_test_new.sh
+++ run_test_new.sh
@@ -21,11 +21,11 @@ jmeter -n -t $1.jmx -l $1_${rName}_result.csv
 echo "*******************Test Complete*******************"
 
 # Copying test results to S3 bucket using AWS API Gateway service - Option-1
-tar -czf $1.tar.gz $1_${rName}_result.csv
-curl --location --request PUT https://h45evhjgs3.execute-api.us-east-1.amazonaws.com/dev/$2/${rName}_test_result.tar.gz --header 'Content-Type: application/gzip' --data-binary @${curr_path}/$1.tar.gz
+# tar -czf $1.tar.gz $1_${rName}_result.csv
+# echo "*******************Copying Results into S3 bucket*******************"
 # curl --location --request PUT https://<AP_GATEWAY_STAGE_URL>$2/${rName}_test_result.tar.gz --header 'Content-Type: application/gzip' --data-binary @${curr_path}/$1.tar.gz
 
 # Copying test results to S3 bucket using AWS CLI command - Option-2
-# yum install awscli -y
-# echo "*******************Copying Results into S3 bucket*******************"
-# aws s3 cp $1_${rName}_result.csv s3://$2/$1_${rName}_result.csv
+yum install awscli -y
+echo "*******************Copying Results into S3 bucket*******************"
+aws s3 cp $1_${rName}_result.csv s3://$2/$1_${rName}_result.csv
