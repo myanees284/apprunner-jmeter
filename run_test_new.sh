769bbc09ad2a10c97dda4ff96685851d6f998202
EC2 Default User <ec2-user@ip-13-0-0-193.us-east-2.compute.internal>
2022-09-02 03:45:09 +0000

    new change

--- run_test_new.sh
+++ run_test_new.sh
@@ -4,19 +4,22 @@ echo "*******************Downloading JMeter*******************"
 curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
 yum -y install tar
 tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
+
 export JMETER_HOME=/opt/apache-jmeter-5.5
 export JMETER_BIN=${JMETER_HOME}/bin
 export PATH=$PATH:$JMETER_BIN
 export curr_path=${PWD}
-echo "*******************Starting load test*******************"
-jmeter -n -t $1.jmx -l $1_result.csv
-echo "*******************Test Complete*******************"
-tar -czf $1.tar.gz $1_result.csv
-curl --location --request PUT https://h45evhjgs3.execute-api.us-east-1.amazonaws.com/dev/$2/${rName}_test_result.tar.gz --header 'Content-Type: application/gzip' --data-binary @${curr_path}/$1.tar.gz
 
-# yum install awscli -y
-# echo "*******************Copying Results into S3 bucket*******************"
-# aws s3 cp $1/ s3://loveisair/$1-$rName-html-report --recursive
+echo "*******************Starting load test*******************"
+jmeter -n -t $1.jmx -l $1_${rName}_result.csv
+echo "*******************Test Complete*******************"
+
+# tar -czf $1.tar.gz $1_${rName}_result.csv
+# curl --location --request PUT https://h45evhjgs3.execute-api.us-east-1.amazonaws.com/dev/$2/${rName}_test_result.tar.gz --header 'Content-Type: application/gzip' --data-binary @${curr_path}/$1.tar.gz
+
+yum install awscli -y
+echo "*******************Copying Results into S3 bucket*******************"
+aws s3 cp $1_${rName}_result.csv/ s3://$2/$1_${rName}_result.csv --recursive
 
 yum -y install nc
 while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo "Hello World"; } | nc -l 8080; done &
